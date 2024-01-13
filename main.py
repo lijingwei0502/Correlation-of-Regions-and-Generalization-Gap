c653cad5aaca4a6d86d5ce5b481d2f8eef644cea
ljw <lijingwei0502@gmail.com>
2024-01-13 08:36:24 +0000

    1

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from projection import generalize_cal_dist, generalize_cal_proj
 import pynvml
 
 def found_device():
-    default_device=0
+    default_device=2
     default_memory_threshold=500
     pynvml.nvmlInit()
     while True:
